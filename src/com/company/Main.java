1442fea61c0290b29ac8c17e0fb806727ab682e7
jroberts3136 <Jroberts3136@gmail.com>
2017-09-25 08:47:45 -0400

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner keyInput = new Scanner(System.in);
+        System.out.println("Enter you string.");
+        String userString = keyInput.nextLine();
+        int stringLength = userString.length();
+
+        int startType = (int)(Math.random() * 5);
+        /*
+        0 - Binary
+        1 - Octal
+        2 - Decimal
+        3 - Hexadecimal
+        4 - ASCII
+        */
+        int startPosition = (int)(((Math.random() * 100) / 100) * stringLength);
+
+        //String[] binary = {}
     }
 }
