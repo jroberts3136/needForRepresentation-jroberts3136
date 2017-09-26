04546a72de0beaff531509275e61bf36e1df0510
jroberts3136 <jroberts3136@gmail.com>
2017-09-25 20:41:38 -0400

    Set up character arrays for searching input string

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,16 +10,21 @@ public class Main {
         String userString = keyInput.nextLine();
         int stringLength = userString.length();
 
+        char[] lowerAlpha = "abcdefghijklmnopqrstuvwxyz".toCharArray();    //+97 to ascii code
+        char[] upperAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();    //+65 to ascii code
+        char[] nonAlpha1 = " !\"#$%&'()*+,-./0123456789:;<=>?@".toCharArray();    //+33 to ascii code
+        char[] nonAlpha2 = "[\\]^_`".toCharArray();    //+91 to ascii code
+        char[] nonAlpha3 = "{|}~".toCharArray();    //+123 to ascii code
+
         int startType = (int)(Math.random() * 5);
         /*
-        0 - Binary
-        1 - Octal
-        2 - Decimal
-        3 - Hexadecimal
-        4 - ASCII
+        0 - Binary          Integer.toBinaryString();
+        1 - Octal           Integer.toOctalString();
+        2 - Decimal         Base ascii code
+        3 - Hexadecimal     Integer.toHexString();
+        4 - ASCII           Character
         */
         int startPosition = (int)(((Math.random() * 100) / 100) * stringLength);
 
-        //String[] binary = {}
     }
 }
